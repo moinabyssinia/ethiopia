8e62d4cab4911e34b6c07c984ef6ca529d945ddf
Michael Tadesse <michaelg.tadesse@gmail.com>
2021-01-27 13:38:29 -0500

    here to help php

---
+++ connection-center/today.php
@@ -0,0 +1,62 @@
+[minti_table style="3"]</p>
+<table width="100%" 
+    style="text-align: left; 
+    border:3px solid black; 
+    border-radius: 5px; 
+    order-collapse:initial;">
+<thead>
+<tr>
+<th style="background:#1279be; padding: 5px 15px;" width="30%" >Event</th>
+<th style="background:#1279be; padding: 5px 15px;" width="30%">Location</th>
+<th style="background:#1279be; padding: 5px 15px;" width="30%">Time</th>
+</tr>
+</thead>
+<tbody>
+{% set reg = "" %}
+{% if urlparam.fg %}
+{% set reg = urlparam.fg %}
+{% endif %}
+{% for i in _context %}
+{% if i.StatusDescription matches '/'~reg~'/i' %}
+<tr>
+
+<!-- event column -->
+<td 
+    style="
+        padding: 2px 15px; 
+        font-size:16px; 
+        color:#1279be; 
+        font-weight:800;">
+[minti_icon icon="fa-chevron-right" 
+color="#1279be" size="16px"] {{i.Name}}<br/>
+</td>
+
+<!-- location column -->
+<td 
+    style="
+        padding: 2px 15px; 
+        font-size:16px; 
+        color:#1279be; 
+        font-weight:500;">
+    [minti_icon icon="fa-location-arrow" 
+    color="#1279be" size="16px"] {{i.LocationAddress}}
+</td>
+
+<!-- time column -->
+<td>
+<span 
+    style="
+        font-size:16px; 
+        color:#1279be; 
+        font-weight:300;">
+    [minti_icon icon="fa-clock-o" 
+    color="#1279be" size="16px"] 
+    {{i.OccurrenceStartTime | date("g:ia") }} - {{i.OccurrenceEndTime | date("g:ia") }} ({{i.StatusDescription}})
+</span>
+</td>
+</tr>
+{% endif %}
+{% endfor %}
+</tbody>
+</table>
+[/minti_table]
