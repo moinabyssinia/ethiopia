41743dcc8dd8c4ba5bacca67891eb7b09404c967
moinabyssinia <michaelg.tadesse@gmail.com>
2021-03-23 21:08:30 -0400

    added table format events

--- faith-events/index.js
+++ faith-events/index.js
@@ -1,9 +1,15 @@
 // using allOrigins to bypass same-origin policy problems
+const canvas = document.querySelector('.canvas');
+
 axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent
         ('https://api.serviceu.com/rest/91c1e592-0eec-490f-80bd-b253cdfe8441?nextDays=1&format=json.json')}`)
         .then(data => {
             const dat = JSON.parse(data.data.contents);
             for(let ii of dat) {
-                console.log(ii.Name);
+                console.log(`${ii.EventId} - ${ii.Name} - ${ii.OccurrenceStartTime}`);
+                const newAnchor = document.createElement('h3');
+                newAnchor.textContent = `${ii.EventId} - ${ii.Name} - ${ii.OccurrenceStartTime}`;
+                canvas.append(newAnchor);
             }
-        })
+        })  
+
