74c4ff16509a0bd463cf27de17067622a4d58b7c
Faith Media's Mac Mini 6 <fmm6@Faiths-Mac-mini.local>
2021-03-26 14:40:24 -0400

    added event table

--- faith-events/index.js
+++ faith-events/index.js
@@ -1,15 +1,46 @@
 // using allOrigins to bypass same-origin policy problems
-const canvas = document.querySelector('.canvas');
+const eventTable = document.querySelector('#table-canvas');
 
 axios.get(`https://api.allorigins.win/get?url=${encodeURIComponent
-        ('https://api.serviceu.com/rest/91c1e592-0eec-490f-80bd-b253cdfe8441?nextDays=1&format=json.json')}`)
+        ('https://api.serviceu.com/rest/91c1e592-0eec-490f-80bd-b253cdfe8441?nextDays=3&format=json.json')}`)
         .then(data => {
             const dat = JSON.parse(data.data.contents);
             for(let ii of dat) {
                 console.log(`${ii.EventId} - ${ii.Name} - ${ii.OccurrenceStartTime}`);
-                const newAnchor = document.createElement('h3');
-                newAnchor.textContent = `${ii.EventId} - ${ii.Name} - ${ii.OccurrenceStartTime}`;
-                canvas.append(newAnchor);
+
+                // create table row
+                const newRow = document.createElement('tr');
+
+                // create table columns
+                const idCol = document.createElement('td');
+                idCol.textContent = ii.EventId;   //${ii.Name} - ${ii.OccurrenceStartTime}`;
+
+                const nameCol = document.createElement('td');
+                nameCol.textContent = ii.Name;
+
+                const timeCol = document.createElement('td');
+                timeCol.textContent = ii.OccurrenceStartTime;
+            
+                const locCol = document.createElement('td');
+                locCol.textContent = ii.CategoryList;
+
+                const descCol = document.createElement('td');
+                descCol.textContent = ii.Description;
+
+
+                const breaker = document.createElement('br');
+
+                // append to table row
+                newRow.append(idCol);
+                newRow.append(nameCol);
+                newRow.append(timeCol);
+                newRow.append(locCol);
+                newRow.append(descCol);
+                newRow.append(breaker);
+
+                // apend to dom
+                eventTable.append(newRow);
+
             }
         })  
 
